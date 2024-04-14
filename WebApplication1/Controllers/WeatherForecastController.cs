dddb426385d53cb7b904c5117f685482a9e8303a
Lucky Solanki <lucky.solanki@beyondkey.com>
2024-04-14 11:36:00 +0530

    test

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers
 {
+    // Updated Commit
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
